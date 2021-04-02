cbe796659340693820d218257abf100aabc71963
get543 <66203118+get543@users.noreply.github.com>
2021-04-02 22:28:29 +0700

    ffmpeg package install

--- commands/play.js
+++ commands/play.js
@@ -1,5 +1,6 @@
 const ytdl = require('ytdl-core');
 const ytSearch = require('yt-search');
+// const ffmpeg = require('ffmpeg-binaries')
 
 //Global queue for your bot. Every server will have a key and value pair in this map. { guild.id, queue_constructor{} }
 const queue = new Map();
