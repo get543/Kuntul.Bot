c46ff8ce13da170e09c5a2ae1ccc78d5f0603963
get543 <66203118+get543@users.noreply.github.com>
2021-04-03 22:02:56 +0700

    Update hei.js

--- commands/hei.js
+++ commands/hei.js
@@ -2,6 +2,12 @@ module.exports = {
     name: 'hei',
     description: "say hello command",
     execute(message, args, client) {
-        message.channel.send(`apa oi`);
+
+        message.channel.send(`apa oi`).then((msg) => {
+            setTimeout(() => msg.delete(), 1000);
+            setTimeout(() => message.delete(), 10000);
+        }).catch((err) => {
+            throw err;
+        })
     }
 }
