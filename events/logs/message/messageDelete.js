e4d92b1346f05547d2f12184f3eeb52795f73f67
ghazapratama <fghaza.pratama@gmail.com>
2022-03-27 22:28:59 +0700

    attachments delete

--- events/logs/message/messageDelete.js
+++ events/logs/message/messageDelete.js
@@ -1,13 +1,21 @@
 module.exports = async (client, Discord) => {
   client.on("messageDelete", (message) => {
-    const deleteEmbed = new Discord.MessageEmbed()
-      .setColor("FF2626")
-      .setTitle("Deleted Message")
-      .addField("Author", `${message.author}`)
-      .addField("Channel", `${message.channel.name}`)
-      .setDescription(message.cleanContent)
-      .setTimestamp();
+    const deleteChannel  = client.channels.cache.get("877518005666471977");
 
-    client.channels.cache.get("877518005666471977").send(deleteEmbed);
+    if (message.attachments.first()) {
+      const picture = message.attachments.map((a) => a.proxyURL);
+
+      deleteChannel.send(`**Deleted By:** ${message.author} **Channel:** ${message.channel} \n${picture}`);
+    } else {
+      const deleteEmbed = new Discord.MessageEmbed()
+        .setColor("FF2626")
+        .setTitle("Deleted Message")
+        .addField("Author", `${message.author}`)
+        .addField("Channel", `${message.channel.name}`)
+        .setDescription(message.cleanContent)
+        .setTimestamp();
+
+      deleteChannel.send(deleteEmbed);
+    }
   });
 };
