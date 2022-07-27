858e3a1c84e5e516d9a263473a1334b7e096ef75
ghazapratama <fghaza.pratama@gmail.com>
2022-07-27 14:55:27 +0700

    attachment logs on embed

--- logs/message/messageDelete.js
+++ logs/message/messageDelete.js
@@ -1,13 +1,33 @@
 module.exports = async (client, Discord) => {
   client.on("messageDelete", (message) => {
-    const deleteChannel = client.channels.cache.get(process.env.DELETE_CHANNEL_ID);
+    const deleteChannel = client.channels.cache.get(
+      process.env.DELETE_CHANNEL_ID
+    );
 
     if (message.attachments.first()) {
-      const picture = message.attachments.map((a) => a.proxyURL);
+      const firstPicture = message.attachments.first().proxyURL;
 
-      deleteChannel.send(`**Deleted By:** ${message.author} **Channel:** ${message.channel} \n${picture}`);
+      const deleteEmbed = new Discord.MessageEmbed()
+        .setColor("FF2626")
+        .setTitle("Attachment Deleted")
+        .addField("Author", `${message.author}`)
+        .addField("Channel", `${message.channel.name}`)
+        .setImage(`${firstPicture}`)
+        .setDescription(
+          message.attachments.map((picture) => `- ${picture.proxyURL}`)
+        )
+        .setTimestamp()
+        .setFooter("Picture shown is the first deleted picture");
+
+      deleteChannel.send(deleteEmbed);
     } else {
-      const deleteEmbed = new Discord.MessageEmbed().setColor("FF2626").setTitle("Deleted Message").addField("Author", `${message.author}`).addField("Channel", `${message.channel.name}`).setDescription(message.cleanContent).setTimestamp();
+      const deleteEmbed = new Discord.MessageEmbed()
+        .setColor("FF2626")
+        .setTitle("Deleted Message")
+        .addField("Author", `${message.author}`)
+        .addField("Channel", `${message.channel.name}`)
+        .setDescription(message.cleanContent)
+        .setTimestamp();
 
       deleteChannel.send(deleteEmbed);
     }
